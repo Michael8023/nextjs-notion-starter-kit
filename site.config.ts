5ff34f489fe8a58cc697a4c78309e3155db810b6
Michael Liu <116956955+Michael8023@users.noreply.github.com>
2024-03-31 20:07:58 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '3d653dde9fdf4e9bbd33c76d10d75f18',
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
